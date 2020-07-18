adaf5c37e75ee67280d376ce72626a212f943de7
cjholl211 <54781247+cjholl211@users.noreply.github.com>
2020-07-18 10:47:36 +0100

    Update firstday_data.py

--- firstday_data.py
+++ firstday_data.py
@@ -2,39 +2,34 @@ import pandas as pd
 from functools import reduce
 from rf_helpers import one_hot_encode
 
-#todo where did demographic come from/ labs
+# data taken from MIMIC-III
 col_list = ['hadm_id', 'admission_type', 'gender', 'age']
-demographic_data = pd.read_csv("C:/Users/Cathal Holloway/OneDrive - University of Birmingham/Masters/module7datastuff/demographic_data.csv", 'r',
-                 delimiter=",", usecols=col_list)
+demographic_data = pd.read_csv("demographic_data.csv", 'r', delimiter=",", usecols=col_list)
 
 # filter those under 18
 demographic_data = demographic_data[demographic_data.age >= 18]
 
 # vitals
 col_list = ['hadm_id', 'heartrate_mean', 'sysbp_mean', 'tempc_mean', 'resprate_mean', 'spo2_mean', 'diasbp_mean', 'meanbp_mean']
-vitals = pd.read_csv("C:/Users/Cathal Holloway/OneDrive - University of "
-                     "Birmingham/Masters/module7datastuff/views/vitalsfirstday.csv", 'r', usecols=col_list,
-                     delimiter=',')
+vitals = pd.read_csv("vitalsfirstday.csv", 'r', usecols=col_list, delimiter=',')
 
 
 col_list = ['hadm_id','bicarbonate_mean', 'bun_mean', 'wbc_mean', 'aniongap_mean', 'platelet_mean']
-labs = pd.read_csv("C:/Users/Cathal Holloway/OneDrive - University of Birmingham/Masters/module7datastuff/views/labs_firstday_mean.csv",
+labs = pd.read_csv("labs_firstday_mean.csv",
                    'r', usecols=col_list, delimiter=',')
 
 # gcs
 col_list = ['hadm_id', 'mingcs']
-gca = pd.read_csv("C:/Users/Cathal Holloway/OneDrive - University of Birmingham/Masters/module7datastuff/views/gcsfirstday.csv",
-                  'r', usecols=col_list, delimiter=',')
+gca = pd.read_csv("gcsfirstday.csv", 'r', usecols=col_list, delimiter=',')
 
 # urine
 col_list = ['hadm_id', 'urineoutput']
-urine = pd.read_csv("C:/Users/Cathal Holloway/OneDrive - University of Birmingham/Masters/module7datastuff/views/uofirstday.csv",
-                  'r', usecols=col_list, delimiter=',')
+urine = pd.read_csv("uofirstday.csv", 'r', usecols=col_list, delimiter=',')
 
 col_list = ['hadm_id', 'vent']
-vent = pd.read_csv("C:/Users/Cathal Holloway/OneDrive - University of Birmingham/Masters/module7datastuff/views/ventfirstday.csv",
-                  'r',  usecols=col_list, delimiter=',')
+vent = pd.read_csv("ventfirstday.csv", 'r',  usecols=col_list, delimiter=',')
 
+# merge all dataframes
 data_frames = [gca, vent, urine, labs, vitals]
 
 df = reduce(lambda left, right: pd.merge(left, right, on=['hadm_id'],
